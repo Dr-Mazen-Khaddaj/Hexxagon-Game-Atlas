7258c2fc69126dc941b6d04bea73867af78c92a7
Andrew Wright <aw@ovit.us>
2024-01-31 22:12:01 -0500

    MainFxs.hs functions
    checkIP - check that the initial position contains a piece of the correct player
    checkFP - check that the final position is empty and if the distance is
    1 or 2
    makeM - use checkIP and checkFP to create a new game state

--- src/MainFxs.hs
+++ src/MainFxs.hs
@@ -1,9 +1,26 @@
-module MainFxs (makeMove) where
+module MainFxs (checkFP, checkIP, makeM) where
 
-import DataTypes (Move, Board)
+import Data.Map qualified as Map
+import DataTypes (Board(..), GameInfo(..), GameState(..), Hexagon(..), Move(..), Player(..), Position)
+import UtilityFxs (distance)
 
-checkMove :: Move -> Bool
-checkMove = undefined
+checkIP :: Position -> GameState -> Maybe (Player, Hexagon)
+checkIP ip (Game pt _ (Board b)) = case Map.lookup ip b of
+  Just h -> case pt of 
+    RedPlayer  cs tn -> if h == Red  then Just (RedPlayer  cs tn, Red ) else Nothing
+    BluePlayer cs tn -> if h == Blue then Just (BluePlayer cs tn, Blue) else Nothing
+  _ -> Nothing
 
-makeMove :: Move -> Board -> Board
-makeMove = undefined
+checkFP :: Move -> Board -> Maybe Int
+checkFP m@(Move _ fp) (Board b) = case Map.lookup fp b of
+  Just Empty -> case distance m of
+    2 -> Just 2          
+    1 -> Just 1
+    _ -> Nothing
+  _ -> Nothing
+
+makeM :: Move -> GameInfo -> GameState
+makeM m@(Move ip fp) (GameInfo ps _ gs@(Game _ dl b@(Board mph))) = case (checkIP ip gs, checkFP m b) of
+  (Just (p, h), Just 2) -> Game (head $ filter (/= p) ps) dl . Board . Map.insert fp h $ Map.delete ip mph
+  (Just (p, h), Just 1) -> Game (head $ filter (/= p) ps) dl . Board $ Map.insert fp h mph
+  _ -> gs
