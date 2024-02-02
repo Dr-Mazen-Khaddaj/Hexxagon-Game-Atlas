c7aeb80d3f88788eb6a93cfc01f5d04c00c3967b
Dr. Mazen Khaddaj <MazenKhaddaj@outlook.com>
2024-02-02 19:38:54 +0200

    feat(DataTypes): enhance custom data types with additional instances
    - Expanded the 'DataTypes' module by deriving additional instances for custom data types.
    - Incorporated `PlutusTx.makeIsDataIndexed` to improve serialization and deserialization of custom data types in the 'DataTypes' module.
    - This addition facilitates more efficient on-chain data handling, optimizing smart contract interactions.
    
    The integration marks a significant improvement in how data types are managed within the Plutus framework, ensuring greater compatibility and efficiency in smart contract development.

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module  DataTypes   ( Player            (..)
                     , Hexagon           (..)
                     , Position          (..)
@@ -12,56 +14,75 @@ module  DataTypes   ( Player            (..)
                     ) where
 
 import  PlutusLedgerApi.V2  (CurrencySymbol, TokenName, POSIXTime)
+import  Data.Maybe          (fromJust)
 import  Data.Map            qualified as Map
+import  PlutusTx.IsData     qualified as PlutusTx
+import  PlutusTx.AssocMap   qualified as AssocMap
 
 ------------------------------------------------------ | Data Types | ------------------------------------------------------
 --  Player
 data    Player          = RedPlayer  CurrencySymbol TokenName
                         | BluePlayer CurrencySymbol TokenName
-                        deriving stock Eq
+                        deriving stock (Eq, Show)
+PlutusTx.makeIsDataIndexed ''Player [('RedPlayer, 0),('BluePlayer, 1)]
 
 --  Board
 data    Hexagon         = Empty
                         | Red
                         | Blue
                         deriving stock Eq
+PlutusTx.makeIsDataIndexed ''Hexagon [('Empty, 0),('Red, 1),('Blue, 2)]
 
-data    Position        = Position  { getX :: Int
-                                    , getY :: Int
+data    Position        = Position  { getX :: Integer
+                                    , getY :: Integer
                                     }
                         deriving stock (Show, Ord, Eq)
+PlutusTx.makeIsDataIndexed ''Position [('Position,0)]
 
 type    Block           = (Position,Hexagon)
 
+instance PlutusTx.ToData (Map.Map Position Hexagon) where toBuiltinData = PlutusTx.toBuiltinData . AssocMap.fromList . Map.toList
+instance PlutusTx.FromData (Map.Map Position Hexagon) where fromBuiltinData = (Map.fromList . AssocMap.toList <$>) . PlutusTx.fromBuiltinData
+instance PlutusTx.UnsafeFromData (Map.Map Position Hexagon) where unsafeFromBuiltinData = fromJust . PlutusTx.fromBuiltinData
+
 newtype Board           = Board (Map.Map Position Hexagon)
                         deriving newtype Eq
+PlutusTx.makeIsDataIndexed ''Board [('Board, 0)]
 
 --  Move
 data    Move            = Move      { initialPosition   :: Position
                                     , finalPosition     :: Position
-                                    }
+                                    } deriving stock (Eq, Show)
+PlutusTx.makeIsDataIndexed ''Move [('Move,0)]
 
 --  Game
 data    GameSettings    = Settings  { getPlayer1        :: Player
                                     , getTurnDuration   :: POSIXTime
                                     , getBoardS0        :: Board
-                                    }
+                                    } deriving stock Eq
+PlutusTx.makeIsDataIndexed ''GameSettings [('Settings,0)]
 
 data    GameState       = Game      { getPlayer'sTurn   :: Player
                                     , getDeadline       :: POSIXTime
                                     , getBoard          :: Board
-                                    }
+                                    } deriving stock Eq
+PlutusTx.makeIsDataIndexed ''GameState [('Game,0)]
 
 data    GameInfo        = GameInfo  { getPlayers        :: [Player]
                                     , getTurnDuration'  :: POSIXTime
                                     , getGameState      :: GameState
-                                    }
+                                    } deriving stock Eq
+PlutusTx.makeIsDataIndexed ''GameInfo [('GameInfo,0)]
 
 data    Initialization  = Add Player
                         | Withdraw
+                        deriving (Eq, Show)
+PlutusTx.makeIsDataIndexed ''Initialization [('Add,0),('Withdraw,1)]
 
 data    RunGame         = PlayTurn
                         | GameOver Player
                         | TimeOut
+                        deriving (Eq, Show)
+PlutusTx.makeIsDataIndexed ''RunGame [('PlayTurn,0),('GameOver,1),('TimeOut,2)]
 
 ----------------------------------------------------------------------------------------------------------------------------
