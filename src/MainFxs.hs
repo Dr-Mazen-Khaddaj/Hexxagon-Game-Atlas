be23cee9390c2205b071527abd16df444e7d23ca
Andrew Wright <aw@ovit.us>
2024-02-04 11:50:51 -0500

    MainFXs.hs functions (1)
    checkWinner - check winning conditions
    completeBoard - fill all available empty areas on the board with
                    a player's hexagon
    fillBoard - expand a player's hexagons to empty areas 2 distances away
    makeMove - updated to convert opponent's hexagons 1 distance away upon a legal move
    noMoves - check if a player has no available moves
    score - calculate score

--- src/MainFxs.hs
+++ src/MainFxs.hs
@@ -1,8 +1,8 @@
-module MainFxs (checkFP, checkIP, makeM) where
+module MainFxs (checkFP, checkIP, checkWinner, fillBoard, makeMove, noMoves) where
 
 import Data.Map qualified as Map
 import DataTypes (Board(..), GameInfo(..), GameState(..), Hexagon(..), Move(..), Player(..), Position)
-import UtilityFxs (distance)
+import UtilityFxs (distance, getNearbyPositions)
 
 checkIP :: Position -> GameState -> Maybe (Player, Hexagon)
 checkIP ip (Game pt _ (Board b)) = case Map.lookup ip b of
@@ -19,8 +19,48 @@ checkFP m@(Move _ fp) (Board b) = case Map.lookup fp b of
     _ -> Nothing
   _ -> Nothing
 
-makeM :: Move -> GameInfo -> GameState
-makeM m@(Move ip fp) (GameInfo ps _ gs@(Game _ dl b@(Board mph))) = case (checkIP ip gs, checkFP m b) of
-  (Just (p, h), Just 2) -> Game (head $ filter (/= p) ps) dl . Board . Map.insert fp h $ Map.delete ip mph
-  (Just (p, h), Just 1) -> Game (head $ filter (/= p) ps) dl . Board $ Map.insert fp h mph
-  _ -> gs
+makeMove :: Move -> GameInfo -> Maybe GameState
+makeMove m@(Move ip fp) (GameInfo ps _ gs@(Game _ dl b@(Board mph))) = case (checkIP ip gs, checkFP m b) of
+  (Just (p, h), Just 2) -> Just $ Game (head $ filter (/= p) ps) dl $ 
+                           Board $ foldr 
+                           (\x -> Map.insert x h) 
+                           (Map.insert fp h $ Map.delete ip mph)
+                           (getNearbyPositions b fp (if h == Red then Blue else Red) 1)
+  (Just (p, h), Just 1) -> Just $ Game (head $ filter (/= p) ps) dl $ 
+                           Board $ foldr 
+                           (\x -> Map.insert x h) 
+                           (Map.insert fp h mph)
+                           (getNearbyPositions b fp (if h == Red then Blue else Red) 1)
+  _ -> Nothing
+
+
+noMoves :: Hexagon -> Board -> Bool
+noMoves h b@(Board mph) = null . concat $ (\x -> getNearbyPositions b x Empty 2) <$> (Map.keys $ Map.filter (== h) mph)
+
+fillBoard :: Hexagon -> Board -> Board
+fillBoard h b@(Board mph) = Board . foldr (\x -> Map.insert x h) mph . concat $ (\x -> getNearbyPositions b x Empty 2) <$> (Map.keys $ Map.filter (== h) mph)
+
+completeBoard :: Hexagon -> Board -> Board
+completeBoard h b = case noMoves h b of  
+  False -> completeBoard h $ fillBoard h b 
+  _     -> b
+
+checkWinner :: Board -> Maybe Hexagon
+checkWinner b@(Board mph)
+  | not . elem Blue $ Map.elems mph = Just Red
+  | not . elem Red  $ Map.elems mph = Just Blue
+  | noMoves Red b = case (score $ completeBoard Blue b) of
+        (r, bl) | r > bl -> Just Red
+                | r < bl -> Just Blue
+                | otherwise -> Just Empty
+  | noMoves Blue b = case (score $ completeBoard Red b) of
+        (r, bl) | r > bl -> Just Red
+                | r < bl -> Just Blue
+                | otherwise -> Just Empty
+  | otherwise = Nothing
+
+score :: Board -> (Int, Int)
+score (Board b) = (Map.size $ Map.filter (== Red) b, Map.size $ Map.filter (== Blue) b)
+
+
+
