dd5befc8c8b2d83efb36da861134ea57c7bd8d57
Andrew Wright <aw@ovit.us>
2024-02-14 00:19:26 -0500

    IOFxs.hs
    - Added offchain IO functions
    - Made adjustments to MainFXs.hs
    - Included modules 'haskeline' and 'time' as build dependencies

--- src/IOFxs.hs
+++ src/IOFxs.hs
@@ -1,10 +1,86 @@
 module IOFxs where
-import DataTypes (Board, Position)
 
-printBoard :: Board -> IO ()
-printBoard = undefined
+import Control.Monad.IO.Class (liftIO)
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import DataTypes (Board(..), GameInfo(..), GameState(..), Hexagon(..), Move(..), Player(..), Position)
+import MainFxs (checkFinalPosition, checkInitialPosition, checkWinner, makeMove, parseInput, score)
+import System.Console.Haskeline
+import UtilityFxs (makeStartingBoard) 
 
-getInitialPosition :: IO Position
-getInitialPosition = undefined
+main :: GameInfo -> IO (Either String GameInfo)
+main gi = runInputT defaultSettings $ main' gi
 
--- etc...
+main' :: GameInfo -> InputT IO (Either String GameInfo)
+main' gi@(GameInfo _ _ (Game _ _ b)) = do 
+  outputStrLn "HEXXAGŌN"
+  outputStrLn $ show b
+  scoreIO b
+  winner <- checkWinnerIO gi
+  case winner of
+    Right _ -> checkWinnerIO gi
+    _ -> game gi
+
+game :: GameInfo -> InputT IO (Either String GameInfo)
+game gi@(GameInfo ps dt gs) = do
+  ip <- getInitialPosition gs
+  case ip of
+    Just (ip', p, h) -> do
+      fp <- getFinalPosition gs ip'
+      case fp of
+        Just (fp', d) -> do
+          dl <- checkDL gs
+          case dl of
+            True -> 
+              case makeMove gi ip' p h fp' d of
+                Just (Game pt dl' b) -> do
+                  winner <- checkWinnerIO $ GameInfo ps dt (Game pt dl' b)
+                  case winner of
+                    Right _ -> do; outputStrLn $ show b; scoreIO b; checkWinnerIO gi
+                    _ -> do outputStrLn $ show b; scoreIO b; 
+                            posixtime <- liftIO getPOSIXTime
+                            return . Right . GameInfo ps dt $ Game pt (round posixtime * 1000 + dt) b
+                _ -> return $ Left ""
+            _ -> return $ Left "Deadline surpassed."
+        _ -> return $ Left "Invalid final position."
+    _ -> return $ Left "Invalid initial position."
+
+getInitialPosition :: GameState -> InputT IO (Maybe (Position, Player, Hexagon))
+getInitialPosition gs@(Game pt _ _) = do
+  ip <- getInputLine $ case pt of
+    RedPlayer  _ _ -> "Red initial position: "
+    BluePlayer _ _ -> "Blue initial position: "
+  return $ ip >>= parseInput >>= checkInitialPosition gs
+
+getFinalPosition :: GameState -> Position -> InputT IO (Maybe (Position, Int))
+getFinalPosition (Game pt _ b) ip = do
+  fp <- getInputLine $ case pt of
+    RedPlayer  _ _ -> "Red final position: "
+    BluePlayer _ _ -> "Blue final position: "
+  return $ fp >>= parseInput >>= checkFinalPosition b . Move ip
+
+checkDL :: GameState -> InputT IO Bool
+checkDL (Game _ dl _) = do 
+  posixtime <- liftIO getPOSIXTime
+  if round posixtime * 1000 <= dl then return True else return False
+
+checkWinnerIO :: GameInfo -> InputT IO (Either String GameInfo)
+checkWinnerIO (GameInfo ps dt (Game pt dl b)) = case checkWinner b of
+  Just (Red, b') -> do; outputStrLn "\nRed is winner!\n"; return . Right $ GameInfo ps dt $ Game pt dl b'
+  Just (Blue, b') -> do; outputStrLn "\nBlue is winner!\n"; return . Right $ GameInfo ps dt $ Game pt dl b'
+  Just (Empty, b') -> do; outputStrLn "\nTie!"; outputStrLn ""; return . Right $ GameInfo ps dt $ Game pt dl b'
+  _ -> return $ Left ""
+
+scoreIO :: Board -> InputT IO ()
+scoreIO b = outputStrLn . (\(r,bl) -> "\nR: " <> show r <> "\nB: " <> show bl <> "\n") $ score b
+
+testGI :: GameInfo
+testGI = 
+  GameInfo 
+    [ RedPlayer  "6373" "tnr"
+    , BluePlayer "6373" "tnb"
+    ] 
+    3600000 $ 
+    Game 
+      (RedPlayer "6373" "tnr") 
+      2007885704000 $ 
+      makeStartingBoard 9 []
--- src/MainFxs.hs
+++ src/MainFxs.hs
@@ -1,66 +1,68 @@
-module MainFxs (checkFP, checkIP, checkWinner, fillBoard, makeMove, noMoves) where
+module MainFxs (checkFinalPosition, checkInitialPosition, checkWinner, fillBoard, makeMove, noMoves, parseInput, score) where
 
-import Data.Map qualified as Map
-import DataTypes (Board(..), GameInfo(..), GameState(..), Hexagon(..), Move(..), Player(..), Position)
+import Data.Char (isDigit)
+import DataTypes (Board(..), GameInfo(..), GameState(..), Hexagon(..), Move(..), Player(..), Position(..))
+import PlutusTx.AssocMap qualified as AssocMap                              
 import UtilityFxs (distance, getNearbyPositions)
 
-checkIP :: Position -> GameState -> Maybe (Player, Hexagon)
-checkIP ip (Game pt _ (Board b)) = case Map.lookup ip b of
-  Just h -> case pt of 
-    RedPlayer  cs tn -> if h == Red  then Just (RedPlayer  cs tn, Red ) else Nothing
-    BluePlayer cs tn -> if h == Blue then Just (BluePlayer cs tn, Blue) else Nothing
+parseInput :: String -> Maybe Position
+parseInput s = case words s of
+  [x,y] -> if all isDigit x && all isDigit y then Just $ Position (read x) (read y) else Nothing
   _ -> Nothing
 
-checkFP :: Move -> Board -> Maybe Int
-checkFP m@(Move _ fp) (Board b) = case Map.lookup fp b of
+checkInitialPosition :: GameState -> Position -> Maybe (Position, Player, Hexagon)
+checkInitialPosition (Game pt _ (Board b)) ip = case AssocMap.lookup ip b of
+  Just h -> case pt of 
+    RedPlayer  cs tn -> if h == Red  then Just (ip, RedPlayer  cs tn, Red ) else Nothing
+    BluePlayer cs tn -> if h == Blue then Just (ip, BluePlayer cs tn, Blue) else Nothing
+  _ -> Nothing
+
+checkFinalPosition :: Board -> Move -> Maybe (Position, Int)
+checkFinalPosition (Board b) m@(Move _ fp) = case AssocMap.lookup fp b of
   Just Empty -> case distance m of
-    2 -> Just 2          
-    1 -> Just 1
+    2 -> Just (fp, 2)         
+    1 -> Just (fp, 1)
     _ -> Nothing
   _ -> Nothing
 
-makeMove :: Move -> GameInfo -> Maybe GameState
-makeMove m@(Move ip fp) (GameInfo ps _ gs@(Game _ dl b@(Board mph))) = case (checkIP ip gs, checkFP m b) of
-  (Just (p, h), Just 2) -> Just $ Game (head $ filter (/= p) ps) dl $ 
-                           Board $ foldr 
-                           (\x -> Map.insert x h) 
-                           (Map.insert fp h $ Map.delete ip mph)
-                           (getNearbyPositions b fp (if h == Red then Blue else Red) 1)
-  (Just (p, h), Just 1) -> Just $ Game (head $ filter (/= p) ps) dl $ 
-                           Board $ foldr 
-                           (\x -> Map.insert x h) 
-                           (Map.insert fp h mph)
-                           (getNearbyPositions b fp (if h == Red then Blue else Red) 1)
+makeMove :: GameInfo -> Position -> Player -> Hexagon -> Position -> Int -> Maybe GameState
+makeMove (GameInfo ps _ (Game _ dl b@(Board mph))) ip p h fp d = case d of
+  2 -> Just $ Game (head $ filter (/= p) ps) dl $ 
+       Board $ foldr 
+       (\x -> AssocMap.insert x h) 
+       (AssocMap.insert fp h $ AssocMap.insert ip Empty mph)
+       (getNearbyPositions b fp (if h == Red then Blue else Red) 1)
+  1 -> Just $ Game (head $ filter (/= p) ps) dl $
+       Board $ foldr 
+       (\x -> AssocMap.insert x h) 
+       (AssocMap.insert fp h mph)
+       (getNearbyPositions b fp (if h == Red then Blue else Red) 1)
   _ -> Nothing
 
+checkWinner :: Board -> Maybe (Hexagon, Board)
+checkWinner b@(Board mph)
+  | not . elem Blue $ AssocMap.elems mph = Just (Red, completeBoard Red b)
+  | not . elem Red  $ AssocMap.elems mph = Just (Blue, completeBoard Blue b)
+  | noMoves Red b = case (score $ completeBoard Blue b) of
+        (r, bl) | r > bl -> Just (Red, completeBoard Blue b)
+                | r < bl -> Just (Blue, completeBoard Blue b)
+                | otherwise -> Just (Empty, completeBoard Blue b)
+  | noMoves Blue b = case (score $ completeBoard Red b) of
+        (r, bl) | r > bl -> Just (Red, completeBoard Red b)
+                | r < bl -> Just (Blue, completeBoard Red b)
+                | otherwise -> Just (Empty, completeBoard Red b)
+  | otherwise = Nothing
 
 noMoves :: Hexagon -> Board -> Bool
-noMoves h b@(Board mph) = null . concat $ (\x -> getNearbyPositions b x Empty 2) <$> (Map.keys $ Map.filter (== h) mph)
-
-fillBoard :: Hexagon -> Board -> Board
-fillBoard h b@(Board mph) = Board . foldr (\x -> Map.insert x h) mph . concat $ (\x -> getNearbyPositions b x Empty 2) <$> (Map.keys $ Map.filter (== h) mph)
+noMoves h b@(Board mph) = null . concat $ (\x -> getNearbyPositions b x Empty 2) <$> (AssocMap.keys $ AssocMap.filter (== h) mph)
 
 completeBoard :: Hexagon -> Board -> Board
 completeBoard h b = case noMoves h b of  
   False -> completeBoard h $ fillBoard h b 
   _     -> b
 
-checkWinner :: Board -> Maybe Hexagon
-checkWinner b@(Board mph)
-  | not . elem Blue $ Map.elems mph = Just Red
-  | not . elem Red  $ Map.elems mph = Just Blue
-  | noMoves Red b = case (score $ completeBoard Blue b) of
-        (r, bl) | r > bl -> Just Red
-                | r < bl -> Just Blue
-                | otherwise -> Just Empty
-  | noMoves Blue b = case (score $ completeBoard Red b) of
-        (r, bl) | r > bl -> Just Red
-                | r < bl -> Just Blue
-                | otherwise -> Just Empty
-  | otherwise = Nothing
+fillBoard :: Hexagon -> Board -> Board
+fillBoard h b@(Board mph) = Board . foldr (\x -> AssocMap.insert x h) mph . concat $ (\x -> getNearbyPositions b x Empty 2) <$> (AssocMap.keys $ AssocMap.filter (== h) mph)
 
 score :: Board -> (Int, Int)
-score (Board b) = (Map.size $ Map.filter (== Red) b, Map.size $ Map.filter (== Blue) b)
-
-
-
+score (Board b) = (length . AssocMap.toList $ AssocMap.filter (== Red) b, length . AssocMap.toList $ AssocMap.filter (== Blue) b)
