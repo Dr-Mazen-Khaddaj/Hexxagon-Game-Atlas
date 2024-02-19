8ca099efc6e1a0da04a30debb521f0a6443764be
Dr. Mazen Khaddaj <MazenKhaddaj@outlook.com>
2024-02-19 19:48:03 +0200

    feat(Constants): add new sample board configuration
    - Introduced a new sample board configuration in the `Constants` module to provide additional examples and facilitate testing.
    - This addition enriches the set of predefined boards available for development and demonstration purposes, enhancing the project's resources for developers and testers.
    
    This update expands the utility and versatility of the Constants module, supporting more varied and comprehensive testing scenarios.

--- src/Constants.hs
+++ src/Constants.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 
 module  Constants   ( classicBoard_S9DC3
+                    , classicBoard_S5DC3
                     , thousand
                     , million
                     , billion
@@ -12,6 +13,10 @@ import  UtilityFxs  (makeStartingBoard)
 -- A Classic 9 by 9 board with 3 centered deletions
 classicBoard_S9DC3 :: Board
 classicBoard_S9DC3 = makeStartingBoard 9 [Position 4 5, Position 5 4, Position 6 6]
+
+-- A Classic 5 by 5 board with 3 centered deletions
+classicBoard_S5DC3 :: Board
+classicBoard_S5DC3 = makeStartingBoard 5 [Position 2 3, Position 3 2, Position 4 4]
 
 thousand, million, billion  :: Num n => n
 thousand  =  10 ^ 3
